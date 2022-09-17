f02f0de9eba490614f87a4fe8471dff0ca38e590
vishal1132 <vishals@iitbhilai.ac.in>
2022-09-17 16:53:57 +0530

    complete rustlings

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,7 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -46,6 +45,26 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len()==0{
+            return Err(ParsePersonError::Empty);
+        }
+        let sp:Vec<&str>=s.split(',').collect();
+        if sp.len()!=2{
+            return Err(ParsePersonError::BadLen);
+        }
+        let name=sp[0];
+        if name.trim().is_empty(){
+            return Err(ParsePersonError::NoName);
+        }
+        let age = match sp[1].parse::<usize>() {
+            Ok(age)  => age,
+            Err(e) => return Err(ParsePersonError::ParseInt(e)),
+        };
+
+        Ok(Person{
+            name:name.to_string(),
+            age:age
+        })
     }
 }
 
